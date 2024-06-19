38847963ef917baf8e3b099c9a03d61cb2aef142
viveklakhlani2401 <viveklakhlani123@gmail.com>
2024-06-19 12:52:37 +0530

    Apply authorization and notification rule with tab change 19th june 2024 12:52PM

--- src/api/common.js
+++ src/api/common.js
@@ -53,3 +53,218 @@ export async function GenerateNewPassword(email,password) {
       return responseBody;
     }
 }
+
+// VMS/User/Get/All/38
+// Get All User API
+export async function getUserData(type,cmpid) {
+  // const token = localStorage.getItem("token");
+  const responseBody = {
+    responseData: null,
+    hasError: false,
+    errorMessage: null,
+  };
+  // if (token != null) {
+    try {
+      // const headers = {
+      //   Authorization: `Bearer ${token}`,
+      // };
+      // const response = await axios.get(`${API_URL}/User`, {
+      //   headers: headers,
+      // });
+      const response = await axios.get(`${API_URL}VMS/User/Get/${type}/${cmpid}`);
+    
+      responseBody.responseData = response.data;
+      return responseBody;
+    } catch (error) {
+  
+      responseBody.hasError = true;
+      responseBody.errorMessage = responseBody.errorMessage =
+        error.response?.data?.statusMsg || error.response?.data?.errors;
+      return responseBody;
+    }
+  // } else {
+  
+  //   responseBody.hasError = true;
+  //   responseBody.errorMessage = "something wrong";
+  //   return responseBody;
+  // }
+}
+
+// Get auth rule by user
+export const getUserAuthRole = async (email,role,cmpid) => {
+  // const myCookieValue = localStorage.getItem("token");
+  // const userData = localStorage.getItem("User");
+  const responseBody = {
+    responseData: null,
+    hasError: false,
+    errorMessage: null,
+  };
+  try {
+    // if (myCookieValue != null && userData != null) {
+      const payload = { 
+        "useremail":email,
+        "userrole":role,
+        "cmptransid":cmpid
+      }
+      // const headers = {
+      //   Authorization: `Bearer ${myCookieValue}`,
+      // };
+      // const response = await axios.post(
+      //   `${API_URL}/AuthUser/GetAuthRule`,
+      //   reqObj,
+      //   {
+      //     headers: headers,
+      //   }
+      // );
+      const response = await axios.post(`${API_URL}VMS/AuthUser/GET`,payload);
+   
+      responseBody.responseData = response.data;
+      return responseBody;
+    // } 
+    // else {
+
+    //   responseBody.hasError = true;
+    //   responseBody.errorMessage = "UnAuthorise for add user";
+    //   return responseBody;
+    // }
+  } catch (error) {
+    responseBody.hasError = true;
+    responseBody.errorMessage =
+      error.response?.data?.errorMessage || error.response?.data?.errors;
+    return responseBody;
+  }
+};
+
+// Add auth rule
+export async function postAuthenticationRule(payload) {
+  // const myCookieValue = Cookies.get("token");
+  // const userData = Cookies.get("User");
+  // const myCookieValue = localStorage.getItem("token");
+  // const userData = localStorage.getItem("User");
+  // const storedData = localStorage.getItem("AuthRule");
+  const responseBody = {
+    responseData: null,
+    hasError: false,
+    errorMessage: null,
+  };
+  // if (myCookieValue != null && userData != null && storedData != null) {
+    try {
+      // const headers = {
+      //   Authorization: `Bearer ${myCookieValue}`,
+      // };
+      const response = await axios.post(
+        `${API_URL}VMS/AuthUser/Save`,
+        payload
+      );
+      // const response = await axios.post(
+      //   `${API_URL}/AuthUser/SaveAuthRule`,
+      //   payload,
+      //   {
+      //     headers: headers,
+      //   }
+      // );
+    
+      responseBody.responseData = response.data;
+      return responseBody;
+    } catch (error) {
+      responseBody.hasError = true;
+      responseBody.errorMessage = responseBody.errorMessage =
+        error.response?.data?.statusMsg || error.response?.data?.errors;
+      return responseBody;
+    }
+  // } else {
+  //   responseBody.hasError = true;
+  //   responseBody.errorMessage = "something wrong";
+  //   return responseBody;
+  // }
+}
+
+
+// Get Notification auth rule by user
+export const getUserNotificationRule = async (email,role,cmpid) => {
+  // const myCookieValue = localStorage.getItem("token");
+  // const userData = localStorage.getItem("User");
+  const responseBody = {
+    responseData: null,
+    hasError: false,
+    errorMessage: null,
+  };
+  try {
+    // if (myCookieValue != null && userData != null) {
+      const payload = { 
+        "useremail":email,
+        "userrole":role,
+        "cmptransid":cmpid
+      }
+      // const headers = {
+      //   Authorization: `Bearer ${myCookieValue}`,
+      // };
+      // const response = await axios.post(
+      //   `${API_URL}/AuthUser/GetAuthRule`,
+      //   reqObj,
+      //   {
+      //     headers: headers,
+      //   }
+      // );
+      const response = await axios.post(`${API_URL}VMS/NotificationAuthUser/GET`,payload);
+   
+      responseBody.responseData = response.data;
+      return responseBody;
+    // } 
+    // else {
+
+    //   responseBody.hasError = true;
+    //   responseBody.errorMessage = "UnAuthorise for add user";
+    //   return responseBody;
+    // }
+  } catch (error) {
+    responseBody.hasError = true;
+    responseBody.errorMessage =
+      error.response?.data?.errorMessage || error.response?.data?.errors;
+    return responseBody;
+  }
+};
+
+// Add Notification auth rule
+export async function postNotificationRule(payload) {
+  // const myCookieValue = Cookies.get("token");
+  // const userData = Cookies.get("User");
+  // const myCookieValue = localStorage.getItem("token");
+  // const userData = localStorage.getItem("User");
+  // const storedData = localStorage.getItem("AuthRule");
+  const responseBody = {
+    responseData: null,
+    hasError: false,
+    errorMessage: null,
+  };
+  // if (myCookieValue != null && userData != null && storedData != null) {
+    try {
+      // const headers = {
+      //   Authorization: `Bearer ${myCookieValue}`,
+      // };
+      const response = await axios.post(
+        `${API_URL}VMS/NotificationAuthUser/Save`,
+        payload
+      );
+      // const response = await axios.post(
+      //   `${API_URL}/AuthUser/SaveAuthRule`,
+      //   payload,
+      //   {
+      //     headers: headers,
+      //   }
+      // );
+    
+      responseBody.responseData = response.data;
+      return responseBody;
+    } catch (error) {
+      responseBody.hasError = true;
+      responseBody.errorMessage = responseBody.errorMessage =
+        error.response?.data?.statusMsg || error.response?.data?.errors;
+      return responseBody;
+    }
+  // } else {
+  //   responseBody.hasError = true;
+  //   responseBody.errorMessage = "something wrong";
+  //   return responseBody;
+  // }
+}
---
+++ src/components/common-object/common-object.js
@@ -0,0 +1,57 @@
+export const authObject = [
+    {
+      Task_ID: 1,
+      Task_Subject: "Visitors",
+    },
+    {
+      Task_ID: 2,
+      Task_Subject: "Verify Visitors",
+    },
+    {
+      Task_ID: 3,
+      Task_Subject: "User Settings",
+    },
+    {
+      Task_ID: 4,
+      Task_Subject: "General Settings",
+    }
+  ];
+
+export const finalObject = [
+  {
+      "text": "Visitors",
+      "path": "/visitors",
+      "icon": "ri-user-add-line",
+      "hasAccess": true
+  },
+  {
+      "text": "Verify Visitors",
+      "path": "/verifyvisitors",
+      "icon": "ri-user-follow-line",
+      "hasAccess": true
+  },
+  {
+      "text": "User Settings",
+      "path": "/usersettings",
+      "icon": "ri-user-settings-line",
+      "hasAccess": true
+  },
+  {
+      "text": "Notification",
+      "path": "/notification",
+      "icon": "bell-icon",
+      "hasAccess": true
+  },
+  {
+      "text": "Profile",
+      "path": "/profile",
+      "icon": "settings",
+      "hasAccess": true
+  },
+  {
+      "text": "General Settings",
+      "path": "/settings",
+      "icon": "settings",
+      "hasAccess": true
+  }
+]
