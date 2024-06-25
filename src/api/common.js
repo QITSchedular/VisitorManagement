59a4401756a2f1e4b815b0d3ae2db83f92960227
rani-pat <qit.ranip@gmail.com>
2024-06-25 10:54:08 +0530

    25th chnages

--- src/api/common.js
+++ src/api/common.js
@@ -11,30 +11,11 @@ export async function forgetPasswordChk(email) {
     const payload = {
       e_mail: email,
     };
-
     var apiRes = await axios.post(`${API_URL}VMS/ForgetPasswordOTP`, payload);
     if (apiRes.status == 200) {
       console.log("APIresponse : ", apiRes.data);
       responseBody.hasError = false;
       responseBody.responseData = apiRes.data;
-
-    try {
-        const payload = {
-            "e_mail":email
-        }
-      var apiRes = await axios.post(`${API_URL}VMS/ForgetPasswordOTP`, payload);
-      if (apiRes.status === 200) {
-        console.log("APIresponse : ",apiRes.data);
-        responseBody.hasError = false;
-        responseBody.responseData = apiRes.data;
-        return responseBody;
-      } else {
-        responseBody.errorMessage = "Not Save Data..!!";
-      }
-    } catch (error) {
-      responseBody.errorMessage = responseBody.errorMessage =
-        error.response?.data?.statusMsg || error.response?.data?.errors;
-
       return responseBody;
     } else {
       responseBody.errorMessage = "Not Save Data..!!";
@@ -287,29 +268,24 @@ export const getUserNotificationRule = async (email, role, cmpid) => {
 
 // Add Notification auth rule
 export async function postNotificationRule(payload) {
-
   const responseBody = {
     responseData: null,
     hasError: false,
     errorMessage: null,
   };
 
+  try {
+    const response = await axios.post(
+      `${API_URL}VMS/NotificationAuthUser/Save`,
+      payload
+    );
 
-    try {
-
-      const response = await axios.post(
-        `${API_URL}VMS/NotificationAuthUser/Save`,
-        payload
-      );
-
-    
-      responseBody.responseData = response.data;
-      return responseBody;
-    } catch (error) {
-      responseBody.hasError = true;
-      responseBody.errorMessage = responseBody.errorMessage =
-        error.response?.data?.statusMsg || error.response?.data?.errors;
-      return responseBody;
-    }
+    responseBody.responseData = response.data;
+    return responseBody;
+  } catch (error) {
+    responseBody.hasError = true;
+    responseBody.errorMessage = responseBody.errorMessage =
+      error.response?.data?.statusMsg || error.response?.data?.errors;
+    return responseBody;
   }
-
+}
